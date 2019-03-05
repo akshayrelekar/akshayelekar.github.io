53e39a521413bf5b49f50aedb62ff3a6d45aa409
Akshay Pradip Relekar <relekar.a@husky.neu.edu>
2019-03-05 12:35:27 -0500

    adding owl carousel

--- javascript.js
+++ javascript.js
@@ -2,3 +2,20 @@ window.addEventListener("load", function(){
     var overlay= document.getElementsByClassName("overlay");
     oveelay.className += " hidden"
 })
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
